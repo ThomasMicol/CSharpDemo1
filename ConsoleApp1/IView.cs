f10d97e157f6cb2f1acb508eec05bcb2dc3d1bfd
Thomas Micol <thomasmicol@gmail.com>
2017-07-24 17:48:49 +1200

    Add project files.

---
+++ ConsoleApp1/IView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal interface IView
+    {
+        void Start();
+        void Stop();
+        string GetStarted(string prompt);
+        Int16 getInt(string prompt);
+        void Show<T>(T message);
+
+
+    }
+}
